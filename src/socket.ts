6e305ae28a8e4a45d252214b7c10740bf8e5af2c
Frederik Coster <costerfrederik@gmail.com>
2023-12-14 22:26:20 +0100

    Improved socket.ts
    Added a way to request data manually. Changed order of document

--- src/socket.ts
+++ src/socket.ts
@@ -2,19 +2,24 @@ import { reactive } from 'vue';
 import { io } from 'socket.io-client';
 import { MapDataPoint } from './interfaces/MapData';
 
-// Here our state will be declared
-export const socketState = reactive<{ connected: boolean; mapData: MapDataPoint[] }>({
-    connected: false,
-    mapData: [],
-});
-
 // You can specify here the url for the sockets backend
 const url = 'http://localhost:3000';
 
 // Create the socket connection
 export const socket = io(url);
 
-// Set mapdata when it receives refresh
+// Here our state will be declared
+export const socketState = reactive<{ connected: boolean; mapData: MapDataPoint[] }>({
+    connected: false,
+    mapData: [],
+});
+
+// Set new mapdata when server sends it
 socket.on('refresh_needed', (payload: MapDataPoint[]) => {
     socketState.mapData = payload;
 });
+
+// Manually refresh map data
+export function requestData() {
+    socket.emit('data_request');
+}
