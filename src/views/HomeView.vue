64a302765d8010a7aa36f41ff1a9ab84e6088df8
Frederik Coster <costerfrederik@gmail.com>
2023-12-14 19:13:39 +0100

    Cleaned pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,6 @@
 
 <script setup lang="ts">
 import MapInteractive from '@/components/Home/MapInteractive.vue';
-import { onBeforeUnmount } from 'vue';
-import { socket, socketState } from '@/socket';
-
-onBeforeUnmount(() => {
-    socket.disconnect();
-});
 </script>
 
 <style scoped lang="scss"></style>
