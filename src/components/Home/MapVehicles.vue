0035636fb0e9be1719a2ef13c881536e055905b8
Frederik Coster <costerfrederik@gmail.com>
2023-12-15 03:13:35 +0100

    Made a separate component of vehicle list result

--- src/components/Home/MapVehicles.vue
+++ src/components/Home/MapVehicles.vue
@@ -2,50 +2,44 @@
     <section class="overview__list" :class="{ 'list--collapsed': !sideBarStore.isOpen }">
         <section class="list__filter">
             <h4>Filter</h4>
-            <input class="filter__searchbox" v-model="searchQuery" type="text" name="searchbox" placeholder="Search Vehicles by identifier" />
+            <input class="filter__searchbox" v-model="searchBoxQuery" type="text" name="searchbox" placeholder="Search Vehicles by identifier" />
             <h4 class="filter__title">Your vehicles</h4>
         </section>
-        <template v-if="vehiclesLoading">
-            <article class="list__taxi taxi--disabled">
-                <section class="taxi__top">
-                    <img src="@/assets/time_icon.png" alt="loading icon" height="24" width="24" />
-                    <h4 class="taxi__identifier">Loading your vehicles</h4>
-                </section>
-                <p class="taxi__lastposition">Your linked vehicles are being loaded in.</p>
-            </article>
-        </template>
-        <template v-else-if="!socketState.mapData || !socketState.mapData.length">
-            <article class="list__taxi taxi--disabled">
-                <section class="taxi__top">
-                    <img src="@/assets/notlinked_icon.png" alt="Nothing linked icon" height="24" width="24" />
-                    <h4 class="taxi__identifier">No vehicles linked</h4>
-                </section>
-                <p class="taxi__lastposition">Linked vehicles will be shown here. But you have none.</p>
-            </article>
-        </template>
-        <template v-else-if="!filteredMapObjects.length">
-            <article class="list__taxi taxi--disabled">
-                <section class="taxi__top">
-                    <img src="@/assets/notfound_icon.png" alt="Not found icon" height="24" width="24" />
-                    <h4 class="taxi__identifier">No vehicles found</h4>
-                </section>
-                <p class="taxi__lastposition">Your search query did not have any results.</p>
-            </article>
-        </template>
+        <VehicleResult
+            v-if="vehiclesLoading"
+            icon-name="time_icon.png"
+            identifier="Loading your vehicles"
+            last-position="Your linked vehicles are being loaded in."
+            :disabled="true"
+        ></VehicleResult>
+        <VehicleResult
+            v-else-if="!socketState.mapData || !socketState.mapData.length"
+            icon-name="notlinked_icon.png"
+            identifier="No vehicles linked"
+            last-position="Linked vehicles will be shown here. But you have none."
+            :disabled="true"
+        ></VehicleResult>
+        <VehicleResult
+            v-else-if="!filteredMapObjects.length"
+            icon-name="notfound_icon.png"
+            identifier="No vehicles found"
+            last-position="Your search query did not have any results."
+            :disabled="true"
+        ></VehicleResult>
         <template v-else>
-            <article v-for="mapObject in filteredMapObjects" v-bind:key="mapObject.identifier" class="list__taxi">
-                <section class="taxi__top">
-                    <img v-if="mapObject.position" src="@/assets/taxi_icon.png" alt="taxi icon" height="24" width="24" />
-                    <img v-else src="@/assets/warning_icon.png" alt="warning icon" height="24" width="24" />
-                    <h4 class="taxi__identifier">{{ mapObject.identifier }}</h4>
-                </section>
-                <p class="taxi__lastposition">Last position: {{ lastPositionFormatted(mapObject.position) }}</p>
-            </article>
+            <VehicleResult
+                v-for="mapObject in filteredMapObjects"
+                v-bind:key="mapObject.identifier"
+                :icon-name="mapObject.position ? 'taxi_icon.png' : 'warning_icon.png'"
+                :identifier="mapObject.identifier"
+                :last-position="lastPositionFormatted(mapObject.position)"
+            ></VehicleResult>
         </template>
     </section>
 </template>
 
 <script setup lang="ts">
+import VehicleResult from '@/components/Home/VehicleResult.vue';
 import { socketState } from '@/socket';
 import { onMounted, ref } from 'vue';
 import { computed } from 'vue';
@@ -53,21 +47,21 @@ import { Position, MapDataObject } from '@/interfaces/MapData';
 import { useSideBarStore } from '@/stores/sidebar';
 const sideBarStore = useSideBarStore();
 
-// Searchbar
-const searchQuery = ref('');
+const searchBoxQuery = ref('');
+const vehiclesLoading = ref(true);
+
 const filteredMapObjects = computed(() => {
     return socketState.mapData.filter((mapObject: MapDataObject) => {
-        return mapObject.identifier.toLowerCase().trim().includes(searchQuery.value.toLowerCase().trim());
+        return mapObject.identifier.toLowerCase().trim().includes(searchBoxQuery.value.toLowerCase().trim());
     });
 });
 
-const vehiclesLoading = ref(true);
-
 function lastPositionFormatted(position: Position | undefined) {
     if (!position) {
-        return 'Never';
+        return 'Last position: Never';
     }
-    return new Date(position.datetime).toLocaleString('nl-nl');
+    const datetime = new Date(position.datetime).toLocaleString('nl-nl');
+    return `Last position: ${datetime}`;
 }
 
 onMounted(() => {
@@ -84,7 +78,7 @@ onMounted(() => {
     flex-direction: column;
     gap: 10px;
     width: 325px;
-    max-height: calc(100% - 94px);
+    max-height: calc(100% - 24px);
     height: auto;
     z-index: 1000;
     border-radius: 24px;
@@ -118,43 +112,6 @@ onMounted(() => {
             &:focus {
                 border-color: #007afb;
             }
-        }
-    }
-
-    .list__taxi {
-        border: 2px solid #161a1d;
-        border-radius: 12px;
-        cursor: pointer;
-        min-height: 80px;
-        padding: 12px;
-        display: flex;
-        justify-content: space-between;
-        flex-direction: column;
-        &.taxi--disabled {
-            cursor: initial;
-        }
-
-        &.taxi--focused {
-            background-color: #1c1b24;
-            border-color: #007afb;
-        }
-
-        .taxi__top {
-            display: flex;
-            align-items: center;
-            margin-bottom: 12px;
-            gap: 12px;
-        }
-
-        .taxi__identifier {
-            margin: 0;
-            color: white;
-        }
-
-        .taxi__lastposition {
-            color: #818181;
-            margin: 0;
-            font-size: 13px;
         }
     }
 }
