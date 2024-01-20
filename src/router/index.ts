060f0aaf1dcf5e29f03cca0d4dfeb0b9365e36c4
Emil <s1132776@student.hsleiden.nl>
2024-01-20 14:58:46 +0100

    removed: removed settings
    removed settings from everywhere

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import SettingsView from "../views/SettingsView.vue";
 import DashboardView from "../views/DashboardView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -8,11 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/settings",
-    name: "settings",
-    component: SettingsView,
   },
   {
     path: "/dashboard/:identifier",
