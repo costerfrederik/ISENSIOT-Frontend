a339f123fac2bf0bd3488b2d0d87941d6cfca625
Frederik Coster <costerfrederik@gmail.com>
2024-01-24 13:00:18 +0100

    wip: geofence progress

--- src/components/Dashboard/MapLocation.vue
+++ src/components/Dashboard/MapLocation.vue
@@ -6,7 +6,11 @@
 
 <script setup lang="ts">
 import mapboxgl from 'mapbox-gl';
-import { mergeProps, onMounted, onUnmounted, ref, watch } from 'vue';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { FeatureCollection, Feature, Polygon, MultiPolygon, Position, Point } from 'geojson';
+import { point, booleanPointInPolygon, multiPolygon } from '@turf/turf';
+import { updateFence } from '@/socket';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
 import type { Ref } from 'vue';
 import { MapDataObject } from '@/interfaces/MapData';
 import { useMapStore } from '@/stores/map';
@@ -14,6 +18,7 @@ import { useMapStore } from '@/stores/map';
 const mapStore = useMapStore();
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXNlbnNpb3QiLCJhIjoiY2xybmozbXEzMTQxYTJxbjE5ejMyMml6dyJ9.gsraLO-9tLQRAJYpe8qZtA';
 const mapPlaceHolder: Ref<HTMLElement | null> = ref(null);
+const draw: Ref<MapboxDraw | null> = ref(null);
 
 const props = defineProps({
     identifier: {
@@ -22,7 +27,7 @@ const props = defineProps({
     },
 });
 
-function addMarkerToMap(mapDataObjects: MapDataObject[]) {
+function handleMapDataUpdate(mapDataObjects: MapDataObject[]) {
     const filteredMapDataObject = mapDataObjects.find((mapDataObject: MapDataObject) => {
         return mapDataObject.identifier == props.identifier;
     });
@@ -35,15 +40,18 @@ function addMarkerToMap(mapDataObjects: MapDataObject[]) {
         return;
     }
 
+    // Calculate if mapDataObject is trespassing a fence
+    const longLat = [filteredMapDataObject.position.longitude, filteredMapDataObject.position.latitude];
+    // const isTrespassing = isTrespassingFence(longLat);
+
+    // Create marker
     mapStore.lockedMapObject = filteredMapDataObject;
     const LngLat = new mapboxgl.LngLat(filteredMapDataObject.position.longitude, filteredMapDataObject.position.latitude);
     mapStore.mapInstance.setCenter(LngLat);
 
-    // Create marker
     const markerElement = document.createElement('div');
     markerElement.className = 'marker';
 
-    // Create marker
     const marker = new mapboxgl.Marker(markerElement, {
         scale: 0.6,
     })
@@ -53,6 +61,68 @@ function addMarkerToMap(mapDataObjects: MapDataObject[]) {
     // Add marker to store state
     mapStore.mapMarkers.push(marker);
 }
+
+function getMultiPolygonCoordinates(collection: FeatureCollection) {
+    const multiPolygonCoordinates: Position[][][] = collection.features
+        .filter((feature: Feature) => feature.geometry.type === 'Polygon')
+        .map((feature: Feature) => (feature.geometry as Polygon).coordinates);
+    return multiPolygonCoordinates;
+}
+
+function convertToMultiPolygon(multiPolygonCoordinates: Position[][][]) {
+    const multiPolygonFeature: Feature<MultiPolygon> = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'MultiPolygon',
+            coordinates: multiPolygonCoordinates,
+        },
+    };
+
+    return multiPolygonFeature.geometry;
+}
+
+// Method should insert draw area into database
+function updateFenceArea() {
+    if (!draw.value) {
+        return;
+    }
+
+    const collection: FeatureCollection = draw.value.getAll();
+    const multiPolygonCoordinates = getMultiPolygonCoordinates(collection);
+
+    // Insert drawn fence into database
+    updateFence(props.identifier, multiPolygonCoordinates);
+
+    // const multiPolygon = convertToMultiPolygon(multiPolygonCoordinates);
+
+    // if (!multiPolygon) {
+    //     return;
+    // }
+
+    // console.log(multiPolygon);
+
+    // Insert multiPolygon into database
+
+    // const inside = booleanPointInPolygon(pt, multiPolygon);
+    // console.log(inside);
+}
+
+// function isTrespassingFence(longLat: number[]) {
+//     if (!draw.value) {
+//         return;
+//     }
+
+//     const collection: FeatureCollection = draw.value.getAll();
+//     const multiPolygon = convertToMultiPolygon(collection);
+
+//     if (!multiPolygon) {
+//         return;
+//     }
+
+//     const inside = booleanPointInPolygon(pt, multiPolygon);
+//     console.log(inside);
+// }
 
 onMounted(async () => {
     if (!mapPlaceHolder.value) {
@@ -70,6 +140,21 @@ onMounted(async () => {
         logoPosition: 'bottom-right',
     });
 
+    draw.value = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+            polygon: true,
+            trash: true,
+        },
+        defaultMode: 'draw_polygon',
+    });
+
+    map.addControl(draw.value);
+
+    map.on('draw.create', updateFenceArea);
+    map.on('draw.delete', updateFenceArea);
+    map.on('draw.update', updateFenceArea);
+
     // Add controls to map
     map.addControl(
         new mapboxgl.NavigationControl({
@@ -82,7 +167,7 @@ onMounted(async () => {
     watch(
         () => mapStore.mapData,
         (mapDataObjects: MapDataObject[]) => {
-            addMarkerToMap(mapDataObjects);
+            handleMapDataUpdate(mapDataObjects);
         },
         { immediate: true }
     );
--- src/socket.ts
+++ src/socket.ts
@@ -1,39 +1,57 @@
-import { reactive } from "vue";
-import { io } from "socket.io-client";
-import { MapDataObject } from "./interfaces/MapData";
-import { useMapStore } from "@/stores/map";
-import { Taxi } from "./interfaces/Taxi";
-import { useErrorStore } from "@/stores/error";
-import { FormError } from "@/interfaces/FormError";
+import { reactive } from 'vue';
+import { io } from 'socket.io-client';
+import { MapDataObject } from './interfaces/MapData';
+import { useMapStore } from '@/stores/map';
+import { Taxi } from './interfaces/Taxi';
+import { useErrorStore } from '@/stores/error';
+import { FormError } from '@/interfaces/FormError';
+import { Position } from 'geojson';
 
 // You can specify here the url for the sockets backend
-const url = "http://localhost:3000";
+const url = 'http://localhost:3000';
 
 // Create the socket connection
 export const socket = io(url);
 
 // Here our state will be declared
 export const socketState = reactive({
-  connected: false,
+    connected: false,
 });
 
 // Set new mapdata when server sends it to us
-socket.on("refresh_needed", (payload: MapDataObject[]) => {
-  const mapStore = useMapStore();
-  mapStore.setMapData(payload);
+socket.on('refresh_needed', (payload: MapDataObject[]) => {
+    const mapStore = useMapStore();
+    mapStore.setMapData(payload);
 });
 
 // Manually refresh map data
 export function requestData() {
-  socket.emit("data_request");
+    socket.emit('data_request');
 }
 
 export function createTaxi(newTaxi: Taxi) {
-  socket.emit("taxi_create", newTaxi);
+    socket.emit('taxi_create', newTaxi);
 }
 
-socket.on("taxi_inserted", (response: FormError) => {
-  const formErrorStore = useErrorStore();
+export function updateFence(identifier: string, multiPolygonCoordinates: Position[][][]) {
+    console.log(multiPolygonCoordinates);
 
-  formErrorStore.error = response;
+    const fencesObject = {
+        fences: multiPolygonCoordinates.map((subArray) => ({
+            coordinates: subArray.map((coordinate) => [
+                coordinate[0], // longitude
+                coordinate[1], // latitude
+            ]),
+        })),
+    };
+
+    console.log(fencesObject);
+
+    socket.emit('fence_update', fencesObject);
+}
+
+socket.on('taxi_inserted', (response: FormError) => {
+    const formErrorStore = useErrorStore();
+
+    formErrorStore.error = response;
 });
